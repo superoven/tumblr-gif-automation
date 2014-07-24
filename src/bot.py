84668e85421941488195c47bb775876852d9b89b
Taylor Ritenour <tritenour@ebay.com>
2014-07-24 15:57:04 -0700

    linking in uploading

--- src/bot.py
+++ src/bot.py
@@ -1,33 +1,25 @@
-import requests
-import json
-from urlparse import parse_qs
-from requests_oauthlib import OAuth1Session
-from requests_oauthlib import OAuth1
 import pytumblr
 import simplejson
 
-keyfile = "/home/taylor/.tumblr_keys"
+key_file = "/home/taylor/.tumblr_keys"
+tags_file = "/home/taylor/.tumblr_default_tags"
 
-f = open(keyfile, "r")
-client_key = f.readline().rstrip()
-client_secret = f.readline().rstrip()
-resource_owner_key = f.readline().rstrip()
-resource_owner_secret = f.readline().rstrip()
-f.close()
+with open(key_file, "r") as f:
+    client_key = f.readline().rstrip()
+    client_secret = f.readline().rstrip()
+    resource_owner_key = f.readline().rstrip()
+    resource_owner_secret = f.readline().rstrip()
 
-print client_key
-print client_secret
-print resource_owner_key
-print resource_owner_secret
+with open(tags_file, 'r') as f:
+    tags = f.read().split(',')
 
-"""client = pytumblr.TumblrRestClient(
-    client_key,
-    client_secret,
-    resource_owner_key,
-    resource_owner_secret
-)
 
-derp = client.create_photo('superoven', state="publish", tags=["yolo", "swag"], format="markdown", data=["image.jpg", "image2.jpg"], caption="autopost")
-
-print simplejson.dumps(derp, indent=2)
-"""
+def upload_gif(filename, additional_tags):
+    resp = pytumblr.TumblrRestClient(
+        client_key,
+        client_secret,
+        resource_owner_key,
+        resource_owner_secret
+    ).create_photo('superoven', state="draft", tags=tags + additional_tags,
+                               format="markdown", data=[filename])
+    print simplejson.dumps(resp, indent=2)
