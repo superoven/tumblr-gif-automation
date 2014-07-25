efea94bf855610d0cf74795a8a094dbc6052d13a
Taylor Ritenour <tritenour@ebay.com>
2014-07-24 17:43:49 -0700

    improved tests

--- test/basic.py
+++ test/basic.py
@@ -16,16 +16,18 @@ invalid_input_values = [
 ]
 
 
-@pytest.mark.parametrize('inputs', valid_input_values)
-def test_working_cases(inputs):
+def _test_basic(inputs, correct_output):
     with open(fifo, "r") as f:
         ret, filename = output(inputs[0], inputs[1], f)
-        assert ret == 0
-        call(["rm", "-f", filename])
+        assert ret == correct_output
+    return filename
+
+
+@pytest.mark.parametrize('inputs', valid_input_values)
+def test_working_cases(inputs):
+    call(["rm", "-f", _test_basic(inputs, 0)])
 
 
 @pytest.mark.parametrize('inputs', valid_input_values)
 def test_failing_cases(inputs):
-    with open(fifo, "r") as f:
-        ret, filename = output(inputs[0], inputs[1], f)
-        assert ret == 1
+    _test_basic(inputs, 1)
