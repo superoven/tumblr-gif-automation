e899ebd95059ec3f0c73b57612a4fd3ba7d62e2c
Taylor Ritenour <taylorritenour@berkeley.edu>
2014-08-05 22:24:35 -0700

    manage tags

--- src/process.py
+++ src/process.py
@@ -4,6 +4,7 @@ from shell_calls import make_temp_dir, rip_images, make_gif, open_gif
 from config import TAGS_FILE, OUTPUT_DIRECTORY, BASE_DIRECTORY
 import uuid
 from bot import upload_gif
+import json
 
 
 class Engine:
@@ -28,7 +29,7 @@ class Engine:
                     self.vals = []
                     fail_status, output_filename = output(first, second, self.filename)
                     if not fail_status:
-                        tag(output_filename)
+                        tag(self.filename, output_filename)
                     return
 
     def kill(self):
@@ -36,21 +37,37 @@ class Engine:
         self.p.kill()
 
 
+def get_title(filename):
+    import re
+    m = re.search('^.*\[.+\](.+) \- [0-9]+.*$', filename)
+    return m.group(1).lstrip().rstrip()
+
+
 def clean_tags(s):
     return map(lambda x: x.lstrip().rstrip(),s.split(','))
 
-with open(TAGS_FILE, 'r') as f:
-    default_tags = clean_tags(f.read())
+
+def get_tags(title):
+    with open(TAGS_FILE, 'r+') as f:
+        tags = json.loads(f.read().decode('UTF8'))
+        if title in tags:
+            return tags[title] + tags['default']
+        else:
+            print "The show: \"%s\" needs default tags." % title
+            tags[title] = clean_tags(raw_input("Enter comma separated tags:"))
+            f.seek(0)
+            f.write(json.dumps(tags, ensure_ascii=False, encoding='utf8').encode("UTF8"))
+            f.truncate()
+            return tags[title] + tags['default']
 
 
-def tag(output_filename):
-    #TODO: Infer basic tags, get translations of names
+def tag(filename, output_filename):
     if not yes_or_no("Want to save and tag this image for upload? [y/n]: "):
         call(["rm", "-f", output_filename])
         print "Yeah, I didn't like that one either."
         return
     additional_tags = raw_input("Type comma separated tags: ")
-    upload_gif(output_filename, default_tags + clean_tags(additional_tags))
+    upload_gif(output_filename, clean_tags(additional_tags) + get_tags(get_title(filename)))
 
 
 def output(first, second, filename):
